8a6ee45397a21fce0a0c6f8ad22eef226c97e24d
SeppeGG <seppe.gevaert@leerling.de-passer.be>
2025-03-18 09:59:56 +0100

    De basis

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    fetchIp();
+    fetchGeoIp();
+    document.querySelector('#ButtonRefresh').addEventListener('click', refresh);
+}
+
+async function fetchIp() {
+    try{
+        let data = await fetch("https://api.ipify.org/?format=json");
+        let ip = await data.json();
+        displayIp(ip);
+    }
+    catch(error){
+        console.log("Fout bij het ophalen van de api ",error);
+    }
+}
+
+function displayIp(ip){
+    const container = document.querySelector("#IpContainer");
+    container.innerHTML = ip.ip;
+}
+
+async function fetchGeoIp(){
+    try {
+        let response = await fetch("https://ipinfo.io/195.130.157.67/geo");
+        let data = await response.json();
+        displayCity(data);
+        displayRegion(data);
+        displayCountry(data);
+        displayLoc(data);
+    }
+    catch(error){
+        console.log("Fout bij het ophalen van de api ",error);
+    }
+}
+
+function displayCity(data){
+    const container = document.querySelector("#CityContainer");
+    container.innerHTML = data.city;
+}
+function displayRegion(data){
+    const container = document.querySelector("#RegionContainer");
+    container.innerHTML = data.region;
+}
+function displayCountry(data){
+    const container = document.querySelector("#CountryContainer");
+    container.innerHTML = data.country;
+}
+
+function displayLoc(data){
+    const container = document.querySelector("#LocContainer");
+    container.innerHTML = data.loc;
+}
+
+function refresh() {
+    fetchIp();
+    fetchGeoIp();
+}
